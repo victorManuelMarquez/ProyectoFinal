c17d393a078d274539e5b5f12416579c9ea02d43
victor <victormmla3lm@gmail.com>
2025-01-18 18:02:00 -0300

    Better logic in connection setup

---
+++ src/ar/com/elbaden/gui/ConnectionPrefab.java
@@ -0,0 +1,95 @@
+package ar.com.elbaden.gui;
+
+import ar.com.elbaden.utils.Strings;
+
+import javax.swing.*;
+import java.awt.*;
+
+public final class ConnectionPrefab extends JPanel {
+
+    private final GridBagConstraints constraints;
+    private final JTextField userField;
+    private final JPasswordField passField;
+    private final JButton showPassBtn;
+
+    public ConnectionPrefab() {
+        super(new GridBagLayout());
+
+        String localUserTxt = "Usuario:";
+        String localPassTxt = "Contraseña:";
+        String localShowTxt = "Mostrar";
+        String localHideTxt = "Ocultar";
+
+        this.constraints = new GridBagConstraints();
+        getConstraints().weightx = 1.0;
+        getConstraints().insets = new Insets(4, 4, 4, 4);
+
+        JLabel userLabel = new JLabel(localUserTxt);
+        JLabel passLabel = new JLabel(localPassTxt);
+
+        Insets fieldMargin = UIManager.getInsets("TextPane.margin");
+
+        this.userField = new JTextField();
+        this.passField = new JPasswordField();
+        this.showPassBtn = new JButton(localShowTxt);
+
+        Strings.fitDynamicContent(showPassBtn, localShowTxt, localHideTxt);
+
+        userLabel.setLabelFor(userField);
+        getConstraints().anchor = GridBagConstraints.LINE_END;
+        add(userLabel, getConstraints());
+
+        userField.setMargin(fieldMargin);
+        getConstraints().anchor = GridBagConstraints.CENTER;
+        getConstraints().fill = GridBagConstraints.HORIZONTAL;
+        getConstraints().gridwidth = GridBagConstraints.REMAINDER;
+        add(userField, getConstraints());
+
+        passLabel.setLabelFor(passField);
+        getConstraints().anchor = GridBagConstraints.LINE_END;
+        getConstraints().fill = GridBagConstraints.NONE;
+        getConstraints().gridwidth = 1;
+        add(passLabel, getConstraints());
+
+        passField.setMargin(fieldMargin);
+        passField.setColumns(12);
+        getConstraints().anchor = GridBagConstraints.CENTER;
+        getConstraints().fill = GridBagConstraints.HORIZONTAL;
+        getConstraints().gridwidth = GridBagConstraints.RELATIVE;
+        add(passField, getConstraints());
+
+        getConstraints().fill = GridBagConstraints.NONE;
+        getConstraints().weightx = 0.0;
+        add(showPassBtn, getConstraints());
+
+        char emptyChar = Character.MIN_VALUE;
+        char defaultChar = passField.getEchoChar();
+
+        showPassBtn.addActionListener(_ -> {
+            if (getPassField().getEchoChar() == defaultChar) {
+                getPassField().setEchoChar(emptyChar);
+                getShowPassBtn().setText(localHideTxt);
+            } else {
+                getPassField().setEchoChar(defaultChar);
+                getShowPassBtn().setText(localShowTxt);
+            }
+        });
+    }
+
+    public GridBagConstraints getConstraints() {
+        return constraints;
+    }
+
+    public JTextField getUserField() {
+        return userField;
+    }
+
+    public JPasswordField getPassField() {
+        return passField;
+    }
+
+    public JButton getShowPassBtn() {
+        return showPassBtn;
+    }
+
+}
--- src/ar/com/elbaden/gui/modal/ConnectionSetUp.java
+++ src/ar/com/elbaden/gui/modal/ConnectionSetUp.java
@@ -2,6 +2,7 @@ package ar.com.elbaden.gui.modal;
 
 import ar.com.elbaden.connection.DataBank;
 import ar.com.elbaden.data.Settings;
+import ar.com.elbaden.gui.ConnectionPrefab;
 import ar.com.elbaden.main.App;
 import ar.com.elbaden.utils.Strings;
 
@@ -19,70 +20,8 @@ public final class ConnectionSetUp extends MasterDialog {
     }
 
     private void installComponents() {
-        String localUserTxt = "Usuario:";
-        String localPassTxt = "Contraseña:";
-        String localShowTxt = "Mostrar";
-        String localHideTxt = "Ocultar";
         String localOk = "Aceptar";
         String localCancel = "Cancelar";
-
-        JLabel labelUser = new JLabel(localUserTxt);
-        JLabel labelPass = new JLabel(localPassTxt);
-
-        Insets fieldMargin = UIManager.getInsets("TextPane.margin");
-
-        int minUserLength = 4;
-        int maxUserLength = 16;
-        JTextField userField = new JTextField();
-        userField.setName(localUserTxt.replace(':', Character.MIN_VALUE));
-        userField.setMargin(fieldMargin);
-        labelUser.setLabelFor(userField);
-
-        Strings.installDocumentFilterValidator(userField, "^(?!\\d)\\w+$", minUserLength, maxUserLength);
-
-        int minPassLength = 8;
-        int maxPassLength = 32;
-        JPasswordField passwordField = new JPasswordField();
-        passwordField.setName(localPassTxt.replace(':', Character.MIN_VALUE));
-        passwordField.setColumns(12);
-        passwordField.setMargin(fieldMargin);
-        labelPass.setLabelFor(passwordField);
-
-        Strings.installDocumentFilterValidator(passwordField, "^\\w+$", minPassLength, maxPassLength);
-
-        JButton showButton = new JButton(localShowTxt);
-        Strings.fitDynamicContent(showButton, localShowTxt, localHideTxt);
-
-        JPanel inputsPanel = new JPanel(new GridBagLayout());
-        add(inputsPanel);
-
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.weightx = 1.0;
-        gbc.insets = new Insets(4, 4, 4, 4);
-
-        gbc.anchor = GridBagConstraints.LINE_END;
-        inputsPanel.add(labelUser, gbc);
-
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        inputsPanel.add(userField, gbc);
-
-        gbc.gridy = 1;
-
-        gbc.anchor = GridBagConstraints.LINE_END;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridwidth = 1;
-        inputsPanel.add(labelPass, gbc);
-
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.gridwidth = GridBagConstraints.RELATIVE;
-        inputsPanel.add(passwordField, gbc);
-
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.weightx = .0;
-        inputsPanel.add(showButton, gbc);
 
         JPanel buttonsPanel = new JPanel();
         add(buttonsPanel, BorderLayout.SOUTH);
@@ -96,12 +35,20 @@ public final class ConnectionSetUp extends MasterDialog {
         JButton cancelBtn = new JButton(localCancel);
         buttonsPanel.add(cancelBtn);
 
-        showButton.addActionListener(_ -> {
-            char newChar = Character.MIN_VALUE;
-            char defaultChar = (Character) UIManager.get("PasswordField.echoChar");
-            passwordField.setEchoChar(passwordField.getEchoChar() == defaultChar ? newChar : defaultChar);
-            showButton.setText(passwordField.getEchoChar() == newChar ? localHideTxt : localShowTxt);
-        });
+        ConnectionPrefab prefab = new ConnectionPrefab();
+        getContentPane().add(prefab);
+
+        int minUserLength = 4;
+        int minPassLength = 8;
+        int maxUserLength = 16;
+        int maxPassLength = 16;
+
+        JTextField userField = prefab.getUserField();
+        JPasswordField passwordField = prefab.getPassField();
+
+        Strings.installDocumentFilterValidator(userField, "^(?!\\d)\\w+$", minUserLength, maxUserLength);
+        Strings.installDocumentFilterValidator(passwordField, "^\\w+$", minPassLength, maxPassLength);
+
         okBtn.addActionListener(_ -> {
             String userValue = userField.getText();
             String passValue = new String(passwordField.getPassword());
@@ -127,6 +74,7 @@ public final class ConnectionSetUp extends MasterDialog {
             }
             dispose();
         });
+
         cancelBtn.addActionListener(_ -> dispose());
     }
 
