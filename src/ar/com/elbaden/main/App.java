e5cac7af2fa7c550363d966a39fccc3bc92216e7
victor <victormmla3lm@gmail.com>
2025-01-21 19:32:16 -0300

    Rebuilding all project

--- src/ar/com/elbaden/main/App.java
+++ src/ar/com/elbaden/main/App.java
@@ -1,27 +1,7 @@
 package ar.com.elbaden.main;
 
-import ar.com.elbaden.data.Settings;
-import ar.com.elbaden.gui.LoadingScreen;
+public class App {
 
-import javax.swing.*;
-import java.util.Properties;
-
-public class App implements Runnable {
-
-    public static Properties properties;
-
-    private App() {
-        properties = new Properties();
-        properties.putAll(Settings.getDefaults());
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new App());
-    }
-
-    @Override
-    public void run() {
-        LoadingScreen.createAndShow();
-    }
+    public static void main(String[] args) {}
 
 }
