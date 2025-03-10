e4d854cbaf12ca21708af5246f76abca6e6eaffd
victor <victormmla3lm@gmail.com>
2025-03-10 10:47:59 -0300

    (g, j, p, q, y) chars skipped

--- src/ar/com/elbaden/gui/MnemonicFinder.java
+++ src/ar/com/elbaden/gui/MnemonicFinder.java
@@ -3,6 +3,7 @@ package ar.com.elbaden.gui;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 public class MnemonicFinder {
 
@@ -12,13 +13,21 @@ public class MnemonicFinder {
         return Character.getNumericValue(value) + 55;
     }
 
+    public boolean ignoreThis(char character) {
+        // caracteres que el mnemonic no puede resaltar correctamente (dependiendo de la fuente)
+        List<Character> characters = List.of('g', 'j', 'p', 'q', 'y');
+        if (!Character.isUpperCase(character)) {
+            return characters.contains(character);
+        } else return false;
+    }
+
     public void setMnemonic(StringBuilder builder, AbstractButton button) {
         if (button.getMnemonic() == KeyEvent.VK_UNDEFINED) {
             boolean mnemonicSet = false;
             int index = 0;
             char[] chars = button.getText().toCharArray();
             while (index < chars.length && !mnemonicSet) {
-                if (builder.toString().indexOf(chars[index]) == -1) {
+                if (builder.toString().indexOf(chars[index]) == -1 && !ignoreThis(chars[index])) {
                     builder.append(chars[index]);
                     button.setMnemonic(mnemonicValue(chars[index]));
                     mnemonicSet = true;
