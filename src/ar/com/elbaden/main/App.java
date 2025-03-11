61ed40cf590cdee010a667a10107f027ff02ede6
victor <victormmla3lm@gmail.com>
2025-03-11 10:22:14 -0300

    Logger with file handler implemented

--- src/ar/com/elbaden/main/App.java
+++ src/ar/com/elbaden/main/App.java
@@ -4,6 +4,13 @@ import ar.com.elbaden.data.Settings;
 import ar.com.elbaden.gui.LoadingScreen;
 
 import javax.swing.*;
+import java.io.IOException;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class App implements Runnable {
 
@@ -11,14 +18,33 @@ public class App implements Runnable {
 
     public static String LOCALES_DIR = "locales/strings";
 
+    private static final Logger LOGGER = Logger.getLogger(App.class.getName());
+
     private App() {
         settings = new Settings();
     }
 
     public static void main(String[] args) {
+        ResourceBundle bundle = null;
         try {
+            bundle = ResourceBundle.getBundle(LOCALES_DIR);
+        } catch (MissingResourceException e) {
+            LOGGER.severe(e.getLocalizedMessage());
+            System.exit(1);
+        }
+        try {
+            FileHandler txtHandler = new FileHandler("%t/java_log.txt", false);
+            txtHandler.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(txtHandler);
+            LOGGER.setLevel(Level.ALL);
+        } catch (IOException e) {
+            LOGGER.severe(bundle.getString("log.cannotSetFileHandler"));
+        }
+        try {
+            LOGGER.info(bundle.getString("log.startingApp"));
             SwingUtilities.invokeLater(new App());
         } catch (RuntimeException e) {
+            LOGGER.severe(e.getLocalizedMessage());
             System.exit(1);
         }
     }
