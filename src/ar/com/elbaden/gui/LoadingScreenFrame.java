cffd37f82c5e3306d8e25731dad6ffce1fc7d4c9
victor <victormmla3lm@gmail.com>
2025-04-11 22:30:35 -0300

    New app checker behaviour

--- src/ar/com/elbaden/gui/LoadingScreenFrame.java
+++ src/ar/com/elbaden/gui/LoadingScreenFrame.java
@@ -1,9 +1,16 @@
 package ar.com.elbaden.gui;
 
+import ar.com.elbaden.task.LaunchAppWorker;
+
 import javax.swing.*;
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
-public class LoadingScreenFrame extends JFrame {
+public class LoadingScreenFrame extends JFrame implements PropertyChangeListener {
+
+    private final JProgressBar progressBar;
+    private final JTextPane outputTextPane;
 
     private LoadingScreenFrame() throws HeadlessException {
         // ajustes
@@ -16,16 +23,42 @@ public class LoadingScreenFrame extends JFrame {
         }
 
         // componentes
-        JScrollPane scrollPane = new JScrollPane();
+        outputTextPane = new JTextPane();
+
+        JScrollPane scrollPane = new JScrollPane(outputTextPane);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         getContentPane().add(scrollPane);
+
+        progressBar = new JProgressBar();
+        progressBar.setStringPainted(true);
+        getContentPane().add(progressBar, BorderLayout.SOUTH);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress".equals(evt.getPropertyName())) {
+            getProgressBar().setValue((Integer) evt.getNewValue());
+        } else if ("cursor".equals(evt.getPropertyName())) {
+            getOutputTextPane().setCursor((Cursor) evt.getNewValue());
+        }
     }
 
     public static void createAndShow() {
         LoadingScreenFrame frame = new LoadingScreenFrame();
+        LaunchAppWorker worker = new LaunchAppWorker(frame.getOutputTextPane());
+        worker.addPropertyChangeListener(frame);
+        frame.addWindowListener(worker);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    public JProgressBar getProgressBar() {
+        return progressBar;
+    }
+
+    public JTextPane getOutputTextPane() {
+        return outputTextPane;
     }
 
 }
