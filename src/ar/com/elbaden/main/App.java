4157eca21bd569485a80938f62c5a84d042a8afd
victor <victormmla3lm@gmail.com>
2025-04-19 13:14:17 -0300

    code cleanup 0003

--- src/ar/com/elbaden/main/App.java
+++ src/ar/com/elbaden/main/App.java
@@ -1,29 +1,9 @@
 package ar.com.elbaden.main;
 
-import ar.com.elbaden.gui.LauncherFrame;
-
-import javax.swing.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class App implements Runnable {
-
-    public static final String FOLDER_NAME = ".baden";
-    public static final String MESSAGES = "i18n/messages";
-
-    private static final Logger GLOBAL_LOGGER = Logger.getGlobal();
-
-    public App() {
-        GLOBAL_LOGGER.setLevel(Level.INFO);
-    }
+public class App {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new App());
-    }
-
-    @Override
-    public void run() {
-        LauncherFrame.createAndShow();
+        System.out.println("Hola, Fin del programa.");
     }
 
 }
