eb729b50bd6c448be81735411c8a94a46a7ab7de
victor <victormmla3lm@gmail.com>
2025-06-15 10:41:03 -0300

    Se agregó la casilla para no preguntar nuevamente para salir del programa.

--- src/ar/com/elbaden/gui/component/ClosingDialog.java
+++ src/ar/com/elbaden/gui/component/ClosingDialog.java
@@ -1,5 +1,6 @@
 package ar.com.elbaden.gui.component;
 
+import ar.com.elbaden.gui.Settings;
 import ar.com.elbaden.main.App;
 
 import javax.swing.*;
@@ -18,30 +19,49 @@ public class ClosingDialog extends ModalDialog {
         FlowLayout flowLayout = new FlowLayout(FlowLayout.TRAILING);
         int hGap = flowLayout.getHgap();
         int vGap = flowLayout.getVgap();
+        flowLayout.setHgap(0);
         flowLayout.setVgap(0);
         int margin = 15;
+        int row = 0;
 
         // componentes
-        JPanel mainPanel = new JPanel(new BorderLayout(hGap, margin));
+        JPanel mainPanel = new JPanel(new GridBagLayout());
         mainPanel.setBorder(BorderFactory.createEmptyBorder(margin, margin, margin, margin));
         JLabel iconLabel = new JLabel(UIManager.getIcon("OptionPane.questionIcon"));
         JLabel messageLabel = new JLabel(App.MESSAGES.getString("closingDialog.message"));
         messageLabel.setHorizontalAlignment(SwingConstants.TRAILING);
-        messageLabel.setBorder(BorderFactory.createEmptyBorder(vGap, hGap, vGap, hGap));
+        messageLabel.setBorder(BorderFactory.createEmptyBorder(vGap, 0, vGap, 0));
+        JCheckBox displayCheckBox = new JCheckBox(App.MESSAGES.getString("closingDialog.doNotAskAgain"));
         JPanel inputPanel = new JPanel(flowLayout);
         JButton exitBtn = new JButton(App.MESSAGES.getString("closingDialog.exitOption"));
+        getRootPane().setDefaultButton(exitBtn);
         JButton cancelBtn = new JButton(App.MESSAGES.getString("closingDialog.cancelOption"));
 
         // instalando componentes
-        getRootPane().setDefaultButton(exitBtn);
-        mainPanel.add(iconLabel, BorderLayout.WEST);
-        mainPanel.add(messageLabel);
         inputPanel.add(exitBtn);
+        inputPanel.add(Box.createHorizontalStrut(margin));
         inputPanel.add(cancelBtn);
-        mainPanel.add(inputPanel, BorderLayout.SOUTH);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(vGap, hGap, vGap, hGap);
+        gbc.gridy = row;
+        gbc.weightx = 1.0;
+        mainPanel.add(iconLabel, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
+        mainPanel.add(messageLabel, gbc);
+        row++;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.gridx = 1;
+        gbc.gridy = row;
+        mainPanel.add(displayCheckBox, gbc);
+        row++;
+        gbc.gridx = GridBagConstraints.RELATIVE;
+        gbc.gridy = row;
+        mainPanel.add(inputPanel, gbc);
         getContentPane().add(mainPanel);
 
         // eventos
+        SwingUtilities.invokeLater(exitBtn::requestFocusInWindow);
         exitBtn.addActionListener(_ -> {
             response = JOptionPane.OK_OPTION;
             dispose();
@@ -54,6 +74,7 @@ public class ClosingDialog extends ModalDialog {
 
     public static int createAndShow(Window owner) {
         ClosingDialog dialog = new ClosingDialog(owner, App.MESSAGES.getString("closingDialog.title"));
+        Settings.updateAllFonts(dialog);
         dialog.pack();
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
