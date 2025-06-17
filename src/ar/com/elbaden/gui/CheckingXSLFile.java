d79fe7db17c7260d82d83a18a2834fde3e071be0
victor <victormmla3lm@gmail.com>
2025-06-17 13:47:16 -0300

    La localización se añade a la verificación y restauración del archivo xsl.

--- src/ar/com/elbaden/gui/CheckingXSLFile.java
+++ src/ar/com/elbaden/gui/CheckingXSLFile.java
@@ -1,25 +1,31 @@
 package ar.com.elbaden.gui;
 
+import ar.com.elbaden.main.App;
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.MessageFormat;
 import java.util.concurrent.ExecutionException;
 
-public class CheckingXSLFile extends CheckPoint<File> {
+public class CheckingXSLFile extends CheckPoint<String> {
 
     private final File xslFile;
+    private final String xslFileFound;
 
     public CheckingXSLFile(File xslFile) {
         this.xslFile = xslFile;
+        // localización
+        xslFileFound = App.MESSAGES.getString("f.checking.file");
     }
 
     @Override
-    public File call() throws Exception {
+    public String call() throws Exception {
         if (Thread.interrupted()) {
             throw new InterruptedException(Thread.currentThread().getName());
         }
         try {
             if (xslFile.exists()) {
-                return xslFile;
+                return MessageFormat.format(xslFileFound, xslFile.getName());
             } else {
                 throw new FileNotFoundException(xslFile.getPath());
             }
--- src/ar/com/elbaden/gui/RestoringXSLFile.java
+++ src/ar/com/elbaden/gui/RestoringXSLFile.java
@@ -1,14 +1,20 @@
 package ar.com.elbaden.gui;
 
+import ar.com.elbaden.main.App;
+
 import java.io.File;
+import java.text.MessageFormat;
 import java.util.concurrent.ExecutionException;
 
 public class RestoringXSLFile extends CheckPoint<String> {
 
     private final File xslFile;
+    private final String xslFileRestored;
 
     public RestoringXSLFile(File xslFile) {
         this.xslFile = xslFile;
+        // localización
+        xslFileRestored = App.MESSAGES.getString("f.restoring.file");
     }
 
     @Override
@@ -19,7 +25,7 @@ public class RestoringXSLFile extends CheckPoint<String> {
         try {
             Settings settings = new Settings();
             settings.restoreXSL(xslFile);
-            return xslFile.getName();
+            return MessageFormat.format(xslFileRestored, xslFile.getName());
         } catch (Exception e) {
             throw new ExecutionException(e);
         }
