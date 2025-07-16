3d06065580da788753dd3f9671bae83e8ca5700b
victor <victormmla3lm@gmail.com>
2025-07-16 11:03:45 -0300

    Se agregó el componente al panel general del diálogo de configuración, de momento sin funcionalidad.

--- src/ar/com/elbaden/gui/window/SettingsDialog.java
+++ src/ar/com/elbaden/gui/window/SettingsDialog.java
@@ -3,8 +3,12 @@ package ar.com.elbaden.gui.window;
 import ar.com.elbaden.main.App;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.util.logging.Logger;
+
+import static javax.swing.LayoutStyle.*;
+import static javax.swing.GroupLayout.*;
 
 public class SettingsDialog extends ModalDialog {
 
@@ -28,6 +32,14 @@ public class SettingsDialog extends ModalDialog {
         groupLayout.setAutoCreateContainerGaps(true);
         mainPanel.setLayout(groupLayout);
 
+        String generalPaneName = App.messages.getString("settingsDialog.generalPanel.name");
+        JPanel generalPanel = new JPanel();
+        generalPanel.setName(generalPaneName);
+        installTitledBorder(generalPanel);
+
+        String askToExit = App.messages.getString("settingsDialog.askToExit");
+        JCheckBox askToExitBtn = new JCheckBox(askToExit);
+
         JButton okBtn = new JButton(ok);
         okBtn.setActionCommand(ok);
         JButton cancelBtn = new JButton(cancel);
@@ -37,12 +49,19 @@ public class SettingsDialog extends ModalDialog {
 
         // instalando componentes
         getRootPane().setDefaultButton(okBtn);
+        generalPanel.add(askToExitBtn);
         groupLayout.setHorizontalGroup(groupLayout.createSequentialGroup()
-                .addComponent(okBtn)
-                .addComponent(cancelBtn)
-                .addComponent(applyBtn));
+                .addGroup(groupLayout.createParallelGroup()
+                        .addComponent(generalPanel)
+                        .addGroup(groupLayout.createSequentialGroup()
+                                .addPreferredGap(ComponentPlacement.RELATED, PREFERRED_SIZE, Short.MAX_VALUE)
+                                .addComponent(okBtn)
+                                .addComponent(cancelBtn)
+                                .addComponent(applyBtn))));
         groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
-                .addGroup(groupLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                .addGroup(groupLayout.createParallelGroup()
+                        .addComponent(generalPanel))
+                .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
                         .addComponent(okBtn)
                         .addComponent(cancelBtn)
                         .addComponent(applyBtn)));
@@ -54,6 +73,17 @@ public class SettingsDialog extends ModalDialog {
         applyBtn.addActionListener(_ -> dispose());
     }
 
+    private void installTitledBorder(JComponent component) {
+        if (component == null || component instanceof JLabel || component instanceof AbstractButton) {
+            return;
+        }
+        if (component.getName() == null || component.getName().isBlank()) {
+            return;
+        }
+        Border titledBorder = BorderFactory.createTitledBorder(component.getName());
+        component.setBorder(titledBorder);
+    }
+
     public static void createAndShow(Window owner) {
         try {
             String title = App.messages.getString("settingsDialog.title");
@@ -63,6 +93,7 @@ public class SettingsDialog extends ModalDialog {
             dialog.setLocationRelativeTo(owner);
             dialog.setVisible(true);
         } catch (RuntimeException e) {
+            e.printStackTrace(System.err);
             LOGGER.severe(e.getMessage());
         }
     }
